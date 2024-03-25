7c63c4829e7caf44d5c7eaba1c4ea43cbabca7e1
MatiasLLL <matias.larralde97@gmail.com>
2024-03-25 13:03:05 -0700

    add deployment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import svgrPlugin from 'vite-plugin-svgr';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin({
-    // svgr options here
-  })],
-});
+  plugins: [react()],
+  server: {
+    port: 4400,
+  },
+  base: "/"
+})
 
 // https://vitejs.dev/config/
 // export default defineConfig({
